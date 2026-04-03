778560068d8b09f0d40a10058902916c7f10e7d7
Lovson Goma <lovsongoma@example.com>
2026-04-03 14:07:50 +0200

    premier commit

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export class Accueil {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    );
+  }
+}
+
+export default Accueil;
